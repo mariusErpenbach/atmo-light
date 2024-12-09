eaf0d6e827b15487fc0660d9a02c285e6a66e5d5
Hawty1 <marius_erpenbach@web.de>
2024-12-09 02:36:28 +0100

    nextron setup

---
+++ atmo-light/renderer/pages/colorDetectorMode.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function colorDetectorMode() {
+  
+  return (
+    <React.Fragment>
+     <div id="colorDetectorMode">
+    
+     </div>
+    </React.Fragment>
+  )
+}
